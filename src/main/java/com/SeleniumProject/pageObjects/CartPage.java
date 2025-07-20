f9a4c6fc8b742c535ed0df1dccd857984d02431e
rahulsriv <rahulsinhaimuv@gmail.com>
2025-07-20 23:23:33 +0530

    Demo

--- src/main/java/com/SeleniumProject/pageObjects/CartPage.java
+++ src/main/java/com/SeleniumProject/pageObjects/CartPage.java
@@ -5,38 +5,43 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.SeleniumProject.AbstractComponents.AbstractComponents;
 
-public class CartPage extends AbstractComponents{
+import java.time.Duration;
+
+public class CartPage extends AbstractComponents {
 	WebDriver driver;
-	
-	public CartPage(WebDriver driver)
-	{
+
+	public CartPage(WebDriver driver) {
 		super(driver);
-		this.driver=driver;
+		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
 
-	@FindBy(xpath="//li[@class='totalRow']/button")
+	@FindBy(xpath = "//li[@class='totalRow']/button")
 	WebElement checkOutEle;
-	
-	@FindBy(xpath="//div[@class='cartSection']/h3")
+
+	@FindBy(xpath = "//div[@class='cartSection']/h3")
 	List<WebElement> cartProducts;
-	
-	public boolean verifyProductDisplay(String productName)
-	{
-		Boolean match=cartProducts.stream().anyMatch(cartProduct->cartProduct.getText().equalsIgnoreCase(productName));
-		return match;
+
+	public boolean verifyProductDisplay(String productName) {
+		return cartProducts.stream()
+				.anyMatch(cartProduct -> cartProduct.getText().equalsIgnoreCase(productName));
 	}
-	
-	public CheckOutPage goToCheckOutPage()
-	{
-		checkOutEle.click();
+
+	public CheckOutPage goToCheckOutPage() {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.elementToBeClickable(checkOutEle));
+
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", checkOutEle);
+		((JavascriptExecutor) driver).executeScript("arguments[0].click();", checkOutEle);
+
 		return new CheckOutPage(driver);
 	}
-	
-	
 }
