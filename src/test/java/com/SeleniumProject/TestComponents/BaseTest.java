fff9d42ccb81f1368fe3a421902d5b484cdea070
rahulsriv <rahulsinhaimuv@gmail.com>
2025-07-20 20:38:00 +0530

    Fix: Added ChromeOptions and UUID profile to avoid Chrome session conflict in Jenkins

--- src/test/java/com/SeleniumProject/TestComponents/BaseTest.java
+++ src/test/java/com/SeleniumProject/TestComponents/BaseTest.java
@@ -10,18 +10,17 @@ import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Properties;
+import java.util.UUID;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 
 import com.SeleniumProject.pageObjects.LandingPage;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -35,7 +34,7 @@ public class BaseTest {
 	public LandingPage landingPage;
 
 	public WebDriver initializeDriver() throws IOException {
-		// Properties Class
+		// Load browser from properties file
 		Properties prop = new Properties();
 		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")
 				+ "//src//main//java//com//SeleniumProject//resources//GlobalData.properties");
@@ -44,58 +43,57 @@ public class BaseTest {
 
 		if (browsername.equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
+
+			ChromeOptions options = new ChromeOptions();
+			
+			// ✅ Prevent session conflict error in Jenkins
+			options.addArguments("--no-sandbox");
+			options.addArguments("--disable-dev-shm-usage");
+			options.addArguments("--user-data-dir=/tmp/chrome-profile-" + UUID.randomUUID());
+
+			driver = new ChromeDriver(options);
 
 		} else if (browsername.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
 
 		} else if (browsername.equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
 			driver = new EdgeDriver();
-
 		}
+
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.manage().window().maximize();
 		return driver;
 	}
-	
-	public List<HashMap<String,String>> getJsonDataToMap(String filePath) throws IOException
-	{
-		String jsonContent = FileUtils.readFileToString(new File(filePath),StandardCharsets.UTF_8);
-		//Convert String to HashMap
-		
-		ObjectMapper mapper=new ObjectMapper();
-		List<HashMap<String,String>> data = mapper.readValue(jsonContent, new TypeReference<List<HashMap<String,String>>>() {
+
+	public List<HashMap<String, String>> getJsonDataToMap(String filePath) throws IOException {
+		String jsonContent = FileUtils.readFileToString(new File(filePath), StandardCharsets.UTF_8);
+
+		ObjectMapper mapper = new ObjectMapper();
+		return mapper.readValue(jsonContent, new TypeReference<List<HashMap<String, String>>>() {
 		});
-		return data;
-		
-		//{map,map}
-		
 	}
-	
-	public String getScreenshot(String testCaseName,WebDriver driver) throws IOException
-	{
-		TakesScreenshot ts=(TakesScreenshot)driver;
-		File source=ts.getScreenshotAs(OutputType.FILE);
-		File file=new File(System.getProperty("user.dir")+"//Reports"+testCaseName+".png");
+
+	public String getScreenshot(String testCaseName, WebDriver driver) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File file = new File(System.getProperty("user.dir") + "//Reports//" + testCaseName + ".png");
 		FileUtils.copyFile(source, file);
-		return System.getProperty("user.dir")+"//Reports"+testCaseName+".png";
+		return file.getAbsolutePath();
 	}
 
-	
-	@BeforeMethod(alwaysRun=true)
-	public void launchApplication() throws IOException
-	{
-		driver=initializeDriver();
-		landingPage=new LandingPage(driver);
+	@BeforeMethod(alwaysRun = true)
+	public void launchApplication() throws IOException {
+		driver = initializeDriver();
+		landingPage = new LandingPage(driver);
 		landingPage.goTo();
-		
-	}
-	
-	
-	@AfterMethod(alwaysRun=true)
-	public void tearDown()
-	{
-		driver.close();
 	}
 
+	@AfterMethod(alwaysRun = true)
+	public void tearDown() {
+		if (driver != null) {
+			driver.quit(); // 🔄 Prefer quit() over close() to clean all resources
+		}
+	}
 }
